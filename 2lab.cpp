145cda26df96adaa8394b7e208141ba73ed0cac6
InvictusBellatorJr <arturnigma@mail.ru>
2015-11-27 02:02:00 +0300

    3.1

--- 2lab.cpp
+++ 2lab.cpp
@@ -5,5 +5,7 @@ int main()
     cout << "Please, Input A and B, bro: ";
     int a,b;
     cin >>a >>b;
-    cout << "Sum A and B is: " <<(a+b);
+    cout << "Sum A and B is: " <<(a+b);  '\n'
+    cout << "Difference A and B is :" <<(a-b); '\n'
+    cout << "Multiplication A and B is: " <<(a*b); '\n';
 }
